372b78840fe24de671f878f8c28807ae53debbc9
Mike Rhodes <mike.rhodes@gmail.com>
2025-05-12 21:01:55 +0100

    codeexplorer: add think tool

--- codeexplorer.py
+++ codeexplorer.py
@@ -546,6 +546,20 @@ tools = [
             "required": ["path"],
         },
     },
+    {
+        "name": "think",
+        "description": "Use the tool to think about something. It will not obtain new information or make any changes to the repository, but just log the thought. Use it when complex reasoning or brainstorming is needed. For example, if you explore the repo and discover the source of a bug, call this tool to brainstorm several unique ways of fixing the bug, and assess which change(s) are likely to be simplest and most effective. Alternatively, if you receive some test results, call this tool to brainstorm ways to fix the failing tests.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "thought": {
+                    "type": "string",
+                    "description": "Your thoughts.",
+                }
+            },
+            "required": ["thought"],
+        },
+    },
 ]
 
 
@@ -624,6 +638,8 @@ def process_tool_call(tool_name: str, tool_input: Dict[str, str]) -> str:
                 tool_input["path"],
                 tool_input["file_text"],
             )
+        case "think":
+            return tool_input["thought"]
     return f"Error: no tool with name {tool_name}"
 
 
